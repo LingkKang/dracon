0991d7288bc6f2b10064d0fbaeaa0496d739d703
Lingkang <contact@lingkang.dev>
2024-02-23 16:41:50 +0000

    :construction_worker: Deploy Rustdoc via CI

---
+++ docgen.sh
@@ -0,0 +1,13 @@
+rm -rf docs
+
+cargo clean
+
+cargo doc --document-private-items --no-deps
+
+'<meta http-equiv="refresh" content="0; url=client/index.html">' > target/doc/index.html
+
+cp -r target/doc/* ../docs
+
+cargo clean
+
+cd ..
