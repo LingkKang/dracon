e22b6db3ba499f12836d50582ce00465c1cf9f78
Lingkang <contact@lingkang.dev>
2024-02-23 17:02:34 +0000

    :green_heart: Fix Rustdoc deploy CI

--- docgen.sh
+++ docgen.sh
@@ -6,8 +6,6 @@ cargo clean
 
 cargo doc --document-private-items --no-deps
 
-'<meta http-equiv="refresh" content="0; url=client/index.html">' > target/doc/index.html
-
 cp -r target/doc/* ../docs
 
 cargo clean
