9b1c6f4bc8ddd40bce2b958d683b2e2c59aa713f
Lingkang <contact@lingkang.dev>
2024-02-23 17:44:51 +0000

    :bug: Fix Rustdoc generation

--- docgen.sh
+++ docgen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -rf docs
+rm -rf ./docs/*
 
 cargo clean
 
 cargo doc --document-private-items --no-deps
 
-'<meta http-equiv="refresh" content="0; url=client/index.html">' > ./target/doc/index.html
+echo '<meta http-equiv="refresh" content="0; url=raft/index.html">' > ./target/doc/index.html
 
 cp -r target/doc ./docs
 
