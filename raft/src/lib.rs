f17e18f2ad0821bdb496b9b32030bbff8bc56b20
Lingkang <contact@lingkang.dev>
2024-02-23 18:13:47 +0000

    :rotating_light: Fix clippy warning

--- raft/src/lib.rs
+++ raft/src/lib.rs
@@ -3,4 +3,4 @@
 //! See [In Search of an Understandable Consensus Algorithm](https://www.usenix.org/system/files/conference/atc14/atc14-paper-ongaro.pdf)
 //! by **Diego Ongaro** and **John Ousterhout** for more details.
 
-mod node;
+pub mod node;
--- raft/src/node.rs
+++ raft/src/node.rs
@@ -4,27 +4,50 @@ pub struct Node {
     status: Status,
 }
 
+/// By default, a new node is created with [`Status::Follower`] status.
+impl Default for Node {
+    fn default() -> Self {
+        Node::new()
+    }
+}
+
+impl Node {
+    /// Create a new node with [`Status::Follower`] status.
+    pub fn new() -> Self {
+        Node {
+            status: Status::Follower,
+        }
+    }
+
+    /// Get the current status of the node.
+    pub fn status(&self) -> &Status {
+        &self.status
+    }
+
+    /// Check if the node is a leader.
+    pub fn is_leader(&self) -> bool {
+        self.status.is_leader()
+    }
+}
+
 /// All possible status (states) of a Raft node.
 ///
 /// As the word **State** have the same wording with *persistent* and
 /// *volatile state* in Node's data, we use **Status** instead.
 pub enum Status {
-    /// Follower status.
-    FLR,
+    /// Follower status. The default status of a new node.
+    Follower,
 
     /// Candidate status.
-    CDT,
+    Candidate,
 
     /// Leader status.
-    LDR,
+    Leader,
 }
 
 impl Status {
     pub fn is_leader(&self) -> bool {
-        match self {
-            Status::LDR => true,
-            _ => false,
-        }
+        matches!(self, Status::Leader)
     }
 }
 
@@ -33,14 +56,21 @@ mod tests {
     use super::*;
 
     #[test]
+    // Make sure that a new node is created with Follower status.
+    fn test_new() {
+        let node = Node::new();
+        assert!(matches!(node.status(), Status::Follower));
+    }
+
+    #[test]
     fn test_is_leader() {
-        let leader = Status::LDR;
+        let leader = Status::Leader;
         assert!(leader.is_leader());
 
-        let follower = Status::FLR;
+        let follower = Status::Follower;
         assert!(!follower.is_leader());
 
-        let candidate = Status::CDT;
+        let candidate = Status::Candidate;
         assert!(!candidate.is_leader());
     }
 }
