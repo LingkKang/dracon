e5ad82719cd1dca36d79f68728c47a379a648e28
Lingkang <contact@lingkang.dev>
2024-02-26 22:29:23 +0000

    :art: Check line length

--- raft/src/lib.rs
+++ raft/src/lib.rs
@@ -1,6 +1,7 @@
 //! The Raft consensus algorithm implementation.
 //!
-//! See [In Search of an Understandable Consensus Algorithm](https://www.usenix.org/system/files/conference/atc14/atc14-paper-ongaro.pdf)
+//! See [In Search of an Understandable Consensus Algorithm](
+//! https://www.usenix.org/system/files/conference/atc14/atc14-paper-ongaro.pdf)
 //! by **Diego Ongaro** and **John Ousterhout** for more details.
 
 pub mod node;
